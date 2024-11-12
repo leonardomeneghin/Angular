19d06b5d59c25224668f84d9d2e5582709f561fa
lmeneghin <lmeneghin@TOPAZBR0128>
2024-11-12 15:28:12 -0300

    add componente header e angular materi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './Aula01/home/home.component';
+import { HeaderComponent } from "./shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({eventCoalescing: true}), provideAnimationsAsync(),
     //O que eu quero oferecer globalmente, fetchs de HTTP, etc eu posso colocar aqui.
   ]
 };
