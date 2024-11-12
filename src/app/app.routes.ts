12957a37863bb4a234666e0d21b1cac5e996f7b3
lmeneghin <lmeneghin@TOPAZBR0128>
2024-11-12 16:29:32 -0300

    add: list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Paths podem ser adicionados aqui o Routes para criarmos um caminho em nosso componente
+    {
+        path : '',
+        component: ListComponent
+    }
+];
